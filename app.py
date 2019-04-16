f4cf5cc91546b367a3c88ba0baee79a5cca04da6
abdoulakram <nabdoulakram@gmail.com>
2019-04-16 10:09:54 +0000

    first commit

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def envoi():
     auth_token = '75b6f0ce16d7b0b713aaf7d70a11605e'
     client = Client(account_sid, auth_token)
     
-    url = 'https://inputpass.herokuapp.com?sessionid=lasttest2'
+    url = 'https://inputpass.herokuapp.com/?sessionid=lasttest2'
     parsed = urllib.parse.urlparse(url)
     idsession=urllib.parse.parse_qs(parsed.query)['sessionid'][0]
     resultat1=(str(sms_reply(password,idsession))).replace('<?xml version="1.0" encoding="UTF-8"?><Response><Message>',"")
