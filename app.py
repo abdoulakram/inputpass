7e61c28ce341c5c14f26f25cd4318aa07327adf2
abdoulakram <nabdoulakram@gmail.com>
2019-04-17 10:04:04 +0000

    first commit

--- app.py
+++ app.py
@@ -8,9 +8,7 @@ import string
 from flask import Flask, jsonify, request, render_template, Markup, json, session, redirect, url_for
 from twilio.rest import Client
 from fonction import sms_reply
-import sys
-sys.path.append('..')
-from virtual_bank import app as f
+
 
 
 
@@ -32,16 +30,16 @@ def envoi():
     account_sid = 'AC89c7cf15d429617da0f4dbe4ad393744'
     auth_token = '75b6f0ce16d7b0b713aaf7d70a11605e'
     client = Client(account_sid, auth_token)
-    
+    retrievePassWord().args
     url = 'https://inputpass.herokuapp.com/?sessionid=lasttest2'
     parsed = urllib.parse.urlparse(url)
     idsession=urllib.parse.parse_qs(parsed.query)['sessionid'][0]
     resultat1=(str(sms_reply(password,idsession))).replace('<?xml version="1.0" encoding="UTF-8"?><Response><Message>',"")
     resultat2=resultat1.replace('</Message></Response>','')
     message = client.messages.create(
-                              body=resultat2,
+                              body=retrievePassWord().args,
                               from_='whatsapp:+14155238886',
-                              to=f.phone()
+                              to='whatsapp:+221776147852'
                           )
     
     
